8c74625250be6f657dc0ca01dd06d0b9c54763cd
Vsevolod <shpakyn.26@gmail.com>
2023-05-07 03:14:03 +0300

    Security config

---
+++ src/main/java/com/springeshop/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.springeshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+}
