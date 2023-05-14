78393dfa3973ab9bea02550bbb15c2111dc222e6
Vsevolod <shpakyn.26@gmail.com>
2023-05-14 22:10:37 +0300

    bucket

--- src/main/java/com/springeshop/controllers/BucketController.java
+++ src/main/java/com/springeshop/controllers/BucketController.java
@@ -1,14 +1,9 @@
 package com.springeshop.controllers;
 
-import com.springeshop.data.domain.Bucket;
 import com.springeshop.data.domain.PurchasedItem;
-import com.springeshop.data.domain.User;
 import com.springeshop.data.dto.ProductDTO;
-import com.springeshop.repositories.UserRepository;
 import com.springeshop.service.BucketService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,26 +17,30 @@ import java.util.List;
 public class BucketController {
 
     private final BucketService bucketService;
+
     @PostMapping("/{productId}")
-    public String addItemToBucket(@PathVariable Long productId, Principal principal){
+    public String addItemToBucket(@PathVariable Long productId, Principal principal) {
+        if (principal == null) {
+            return "login";
+        }
         String username = principal.getName();
         bucketService.addItemToBucket(productId, username);
-        return "product-list";
+        return "redirect:/products";
     }
 
-    @DeleteMapping("/{itemId}")
-    public String removeItemFromBucket(@PathVariable Long itemId){
+    @PostMapping("/delete/{itemId}")
+    public String removeItemFromBucket(@PathVariable Long itemId) {
         bucketService.removeItemFromBucket(itemId);
-        return "bucket-list";
+        return "redirect:/bucket";
     }
 
     @GetMapping
-    public String getBucketItems(Model model, Principal principal){
-        if(principal == null){
+    public String getBucketItems(Model model, Principal principal) {
+        if (principal == null) {
             return "login";
         }
-        List<ProductDTO> BucketProductDTOs = bucketService.getAll(principal.getName());
-        model.addAttribute("bucket", BucketProductDTOs);
+        List<PurchasedItem> BucketProducts = bucketService.getAll(principal.getName());
+        model.addAttribute("bucket", BucketProducts);
         return "bucket-list";
     }
 }
--- src/main/java/com/springeshop/service/BucketService.java
+++ src/main/java/com/springeshop/service/BucketService.java
@@ -4,22 +4,17 @@ import com.springeshop.data.domain.Bucket;
 import com.springeshop.data.domain.Product;
 import com.springeshop.data.domain.PurchasedItem;
 import com.springeshop.data.domain.User;
-import com.springeshop.data.dto.ProductDTO;
-import com.springeshop.exceptions.UserNotFoundException;
+import com.springeshop.exceptions.ProductNotFoundException;
 import com.springeshop.mappers.ProductMapper;
 import com.springeshop.repositories.BucketRepository;
 import com.springeshop.repositories.ProductRepository;
 import com.springeshop.repositories.PurchasedItemRepository;
 import com.springeshop.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,12 +24,13 @@ public class BucketService {
     private final UserRepository userRepository;
     private final PurchasedItemRepository purchasedItemRepository;
     private final ProductMapper productMapper = ProductMapper.MAPPER;
-    public void addItemToBucket(Long productId, String username){
+
+    public void addItemToBucket(Long productId, String username) {
         User user = userRepository.findFirstByName(username);
-        if(user != null) {
-            Product product = productRepository.findById(productId).orElseThrow(() -> new UserNotFoundException("Product not found"));
+        if (user != null) {
+            Product product = productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException("Product not found"));
             Bucket bucket = user.getBucket();
-            if(bucket == null){
+            if (bucket == null) {
                 bucket = new Bucket();
                 bucket.setUser(user);
                 user.setBucket(bucket);
@@ -52,20 +48,16 @@ public class BucketService {
         }
     }
 
-    public void removeItemFromBucket(Long itemId){
+    public void removeItemFromBucket(Long itemId) {
         purchasedItemRepository.deleteById(itemId);
     }
 
-    public List<ProductDTO> getAll(String username){
+    public List<PurchasedItem> getAll(String username) {
         User user = userRepository.findFirstByName(username);
-        if(user != null) {
+        if (user != null) {
             Bucket bucket = user.getBucket();
-            if(bucket != null) {
-                List<PurchasedItem> purchasedItems = bucket.getPurchasedItems();
-                return productMapper.toProductDTOList(
-                        purchasedItems.stream()
-                                .map(PurchasedItem::getProduct)
-                                .collect(Collectors.toList()));
+            if (bucket != null) {
+                return bucket.getPurchasedItems();
             }
         }
         return null;
