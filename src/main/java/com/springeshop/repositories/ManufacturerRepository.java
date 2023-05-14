b4579558cf9b62093df1a50a0896a389f2351a6b
Vsevolod <shpakyn.26@gmail.com>
2023-05-14 19:41:51 +0300

    ref

--- src/main/java/com/springeshop/repositories/ManufacturerRepository.java
+++ src/main/java/com/springeshop/repositories/ManufacturerRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
-
     Manufacturer findFirstByName(String name);
 }
