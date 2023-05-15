036785ad8b2fec5c8877e9aac66c2feeaf1493b9
Vsevolod <shpakyn.26@gmail.com>
2023-05-15 09:22:03 +0300

    searching with not full word

--- src/main/java/com/springeshop/repositories/ProductRepository.java
+++ src/main/java/com/springeshop/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.springeshop.repositories;
 
 import com.springeshop.data.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +15,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @NonNull
     List<Product> findAll();
 
+    @Query(value = "SELECT p FROM Product p WHERE p.name ILIKE %:searchTerm% OR p.manufacturer.name ILIKE %:searchTerm% OR p.category.name ILIKE %:searchTerm%")
+    List<Product> findBySearchTermFuzzy(@Param("searchTerm") String searchTerm);
 
+    List<Product> findAllByNameIsContaining(String name);
 }
---
+++ src/main/resources/db/migration/V2.2_trm.sql
@@ -0,0 +1,3 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE INDEX idx_manufacturer_name_trgm ON manufacturers USING gin (manufacturer_name gin_trgm_ops);
+CREATE INDEX idx_product_name_trgm ON products USING gin (product_name gin_trgm_ops);
